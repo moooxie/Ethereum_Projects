10b22151d1ac6425720985abf9d833be956006a2
moooxie <moooxie@protonmail.com>
2024-08-02 15:04:00 +0300

    fix login issue

---
+++ IPersonService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace ICD.Base.BusinessServiceContract
+{
+    public interface IPersonService
+    {
+        Task<InsertPersonResult> InsertPersonAsync(InsertPersonRequest request);
+        Task<GetPeopleByspecificationResult> GetBySpecificationAsync(GetPeopleBySpecificationQuery query);
+        Task<DeleteTypeLongResult> DeleteByIdAsync(DeleteTypeLongRequest request);
+        Task<BasePersonResult> UpdatePersonAsync(UpdatePersonRequest request);
+        Task<GetPeopleByKeyResult> GetPeopleByKeyAsync(GetPeopleByKeyQuery query);
+    }
+}
