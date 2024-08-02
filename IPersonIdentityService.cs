c7e502754c78ce63445a6bc22f6a6009de8bb593
moooxie <moooxie@protonmail.com>
2024-08-02 15:03:52 +0300

    update assets

---
+++ IPersonIdentityService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace ICD.Base.BusinessServiceContract
+{
+    public interface IPersonIdentityService
+    {
+        Task<BasePersonIdentityResult> InsertPersonIdentityAsync(InsertPersonIdentityRequest request);
+        Task<GetPersonIdentityResult> GetPersonIdentityByPersonRefAsync(GetPersonIdentityQuery query);
+        Task<DeleteTypeIntResult> DeletePersonIdentityByIdAsync(DeleteTypeIntRequest request);
+        Task<BasePersonIdentityResult> UpdatePersonIdentityByIdAsync(UpdatePersonIdentityRequest request);
+    }
+}
