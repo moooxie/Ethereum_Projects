22f4df63e6e00ef93e7aa143f86c7c1e5ea5b62d
moooxie <moooxie@protonmail.com>
2024-08-02 15:04:24 +0300

    add pagination

---
+++ IPersonTitleService.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace ICD.Base.BusinessServiceContract
+{
+    public interface IPersonTitleService
+    {
+        Task<BasePersonTitleResult> InsertPersonTitleAsync(InsertPersonTitleRequest request);
+        Task<GetPersonTitlesResult> GetPersonTitlesByLanguageRefAsync(GetPersonTitlesQuery query);
+        Task<DeleteTypeIntResult> DeletePersonTitleByIdAsync(DeleteTypeIntRequest request);
+        Task<BasePersonTitleResult> UpdatePersonTitleByIdAsync(UpdatePersonTitleRequest request);
+        Task<GetPersonTitleByKeyResult> GetPersonTitleByKeyAsync(GetPersonTitleByKeyQuery query);
+        //object TestGetPersonProc();
+    }
+}
