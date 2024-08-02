b608ae1abf804f8d3672947f73ded4494a42623f
moooxie <moooxie@protonmail.com>
2024-08-02 15:04:49 +0300

    add error messages

---
+++ ITaxService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICD.Base.BusinessServiceContract
+{
+    public interface ITaxService
+    {
+        Task<BaseTaxResult> InsertTaxAsync(InsertTaxRequest request);
+        Task<InsertTaxByTableNameResult> InsertTaxByTableNameAsync(InsertTaxByTableNameRequest request);
+        Task<GetTaxResult> GetTaxAsync(GetTaxQuery query);
+        Task<BaseTaxResult> UpdateTaxAsync(UpdateTaxRequest request);
+        Task<GetTaxByKeyResult> GetTaxByKeyAsync(GetTaxByKeyQuery query);
+        Task<DeleteTypeIntResult> DeleteTaxByIdAsync(DeleteTypeIntRequest request);
+    }
+}
