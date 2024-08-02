8c6a7525b26f699265c8287950bf3e816acec0b7
moooxie <moooxie@protonmail.com>
2024-08-02 15:04:38 +0300

    update css

---
+++ IPlayerService.cs
@@ -0,0 +1,20 @@
+﻿using ICD.Base.Player.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICD.Base.BusinessServiceContract
+{
+    public interface IPlayerService
+    {
+        Task<InsertPlayerResult> InsertPlayerAsync(InsertPlayerRequest request);
+        Task<GetPlayersByTeamResult> GetplayersInTeamAsync(GetPlayersByTeamQuery query);
+        Task<UpdatePlayerResult> UpdatePlayerAsync(UpdatePlayerRequest request);
+        Task<RemovePlayerFromTeamResult> RemovePlayerFromTeamAsync(RemovePlayerFromTeamRequest request);
+
+
+
+    }
+}
